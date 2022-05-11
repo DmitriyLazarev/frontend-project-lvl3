8e0b13dc970f24b9e9b81470f642e6111f0c0ba7
Dmitry Lazarev <dmitry.lazarev@team.wrike.com>
2022-05-11 10:27:22 +0200

    separate locales to own folder

---
+++ src/assets/locales/ru.js
@@ -0,0 +1,15 @@
+export default {
+  translation: {
+    title: 'RSS агрегатор',
+    description: 'Начните читать RSS сегодня! Это легко, это красиво.',
+    form: {
+      inputLabel: 'Ссылка RSS',
+      submitButton: 'Добавить',
+      errors: {
+        wrongUrl: 'Ссылка должна быть валидным URL.',
+        required: 'Поле должно быть заполнено.',
+        isIncluded: 'RSS уже существует.',
+      },
+    },
+  },
+};
--- src/components/rss.js
+++ src/components/rss.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import i18next from 'i18next';
+import ru from '../assets/locales/ru.js';
 import baseLayout from './layout/base.js';
 import form from './main/form.js';
 
@@ -8,21 +9,7 @@ export default () => {
     lng: 'ru',
     debug: false,
     resources: {
-      ru: {
-        translation: {
-          title: 'RSS агрегатор',
-          description: 'Начните читать RSS сегодня! Это легко, это красиво.',
-          form: {
-            inputLabel: 'Ссылка RSS',
-            submitButton: 'Добавить',
-            errors: {
-              wrongUrl: 'Ссылка должна быть валидным URL.',
-              required: 'Поле должно быть заполнено.',
-              isIncluded: 'RSS уже существует.',
-            },
-          },
-        },
-      },
+      ru,
     },
   }).then(() => {
     const [dataContainer] = baseLayout();
