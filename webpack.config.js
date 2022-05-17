5ab77a021a126d0601e4b3de315a029d80e7e14c
Dmitry Lazarev <dmitry.lazarev@team.wrike.com>
2022-05-17 13:41:18 +0200

    rework webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,20 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-const config = {
-  mode: process.env.NODE_ENV || 'development',
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   entry: './src/index.js',
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -17,22 +23,4 @@ const config = {
       filename: 'index.html',
     }),
   ],
-  module: {
-    rules: [
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: 'asset',
-      },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-    ],
-  },
-};
-
-module.exports = () => {
-  if (isProduction) {
-    config.mode = 'production';
-  } else {
-    config.mode = 'development';
-  }
-  return config;
 };
